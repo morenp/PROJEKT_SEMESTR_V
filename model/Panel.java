f8c8b8b4278b852e77d48276dec9bc558a67d5c1
Marcin Pancerz <marcin2523@o2.pl>
2016-12-03 17:03:33 +0100

    Panels superclass

---
+++ model/Panel.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Panel {
+	
+	private  Parent root = null;
+	private  Scene scene = null;
+	private  Parent ancestorPage = null;
+	private  Scene ancestorScene = null;
+	
+	
+	public Panel(String PanelName)
+	{
+		try {
+			root = FXMLLoader.load(getClass().getResource(Main.Sdevelop+PanelName+"Panel.fxml"));
+			//System.out.println(root);
+			root.setStyle("-fx-background-image: url("+ Main.image +")");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource(Main.Sdevelop+"application.css").toExternalForm());
+	}
+
+	public Scene getScene()
+	{
+		return scene;
+	}
+		
+	//----------------- ANCESTOR ----------------------------------------------
+		public  Parent getAncestorPage() {
+			return ancestorPage;
+		}
+
+		public void setAncestorPage(Parent ancestorPage) {
+			this.ancestorPage = ancestorPage;
+		}
+
+		public Scene getAncestorScene() {
+			return ancestorScene;
+		}
+
+		public void setAncestorScene(Scene ancestorScene) {
+			this.ancestorScene = ancestorScene;
+		}
+	//-------------------------------------------------------------------------	
+	
+}
