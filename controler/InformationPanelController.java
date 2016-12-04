c6d0622a83d6fa29b0e04ca0ff0a15e3b88c5140
Marcin Pancerz <marcin2523@o2.pl>
2016-12-04 22:39:53 +0100

    Basic functions of controller - only view and back button

---
+++ controler/InformationPanelController.java
@@ -0,0 +1,26 @@
+package controler;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class InformationPanelController {
+
+	@FXML
+	private  Button btnBack;
+	
+	@FXML
+	private Text txtWelcomeText;
+	
+
+	public void back(ActionEvent event)
+	{	
+		Stage primaryStage = (Stage)((Node) event.getSource()).getScene().getWindow();
+		primaryStage.setScene(MainController.i.getAncestorScene());
+		primaryStage.show();
+	}
+	
+}
---
+++ controler/TimetablePanelController.java
@@ -0,0 +1,27 @@
+package controler;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class TimetablePanelController {
+
+	@FXML
+	private Text txtWelcomeText;
+	
+	@FXML
+	private Button btnBack;
+	
+	
+	public void back(ActionEvent event)
+	{	
+		Stage primaryStage = (Stage)((Node) event.getSource()).getScene().getWindow();
+		primaryStage.setScene(MainController.tt.getAncestorScene());
+		primaryStage.show();
+	}
+	
+	
+}
