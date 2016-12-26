d466ccbfb49e04e340af7faf2286536341e62cb9
Marcin Pancerz <marcin2523@o2.pl>
2016-12-26 18:03:04 +0100

    Added login method

--- model/Login.java
+++ model/Login.java
@@ -1,10 +1,78 @@
 package model;
 
-public class Login  extends Panel{
+import org.apache.commons.validator.routines.EmailValidator;
 
+import mainClasses.AppUser;
+
+public class Login extends Model{
+
+	AppUser appUser;
+	
+	private String resp;
+	
 	public Login()
 	{
-		super("Login");
+		super();
+		appUser = new AppUser();
+	}
+	
+	public int login(String email, String password)
+	{
+		// WALIDATOR ADRESU EMAIL 
+    	EmailValidator ev = EmailValidator.getInstance();
+    	
+		/*
+			String query= "SELECT XMLELEMENT(\"name\",name) || XMLELEMENT(\"last_name\",last_name) as xml_users from users u where u.user_id = 1";	   
+		    String email="";	    
+		    String password="";	    
+		    query = "SELECT XMLELEMENT(\"email\",email) || XMLELEMENT(\"password\",password) as xml_users from users u where u.user_email = "+email;
+		    */
+		
+	    	
+			if(ev.isValid(email) && !password.equals(""))
+			{
+				//Login's data send to the server
+				this.resp = Main.getClient().login(email, password,"1");
+
+			    if(!this.resp.equals("wrong_password"))
+			    {
+			    	if(this.resp.equals("not_exist"))
+			    	{
+			    		// Email address does not exist
+			    		return -1;
+			    	}
+			    	else		
+			    	{
+			    		//Login Success
+			    		return 1;
+			    	}
+				}
+				else
+				{
+					//Wrong password
+					return -3;
+				}
+			}
+			else
+			{
+				if(!ev.isValid(email))
+				{
+					//Wrong email
+					return -2;
+				}
+				else
+					if(password.equals(""))
+					{
+						//Missing password
+						return -4;
+					}				
+			}
+			return 0;		
 	}
 
+	public String getResp() {
+		return resp;
+	}
+	
+	
 }
