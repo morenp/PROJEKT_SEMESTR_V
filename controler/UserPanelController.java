67ea6df0f06dc660b053c8ffda94bff7f34d294c
Marcin Pancerz <marcin2523@o2.pl>
2016-12-26 17:58:59 +0100

    Added function to edit profile, added observer pattern

--- controler/UserPanelController.java
+++ controler/UserPanelController.java
@@ -1,28 +1,68 @@
 package controler;
 
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import model.Main;
+import model.User;
+import observe.Observer;
 
 
-public class UserPanelController {
+public class UserPanelController extends Controller implements Observer{
 
-	@FXML
-	private Text txtWelcomeText;
-	
-	@FXML
-	private Button btnBack;
-	
-	
-	
-	public void back(ActionEvent event)
-	{	
-		Stage primaryStage = (Stage)((Node) event.getSource()).getScene().getWindow();
-		primaryStage.setScene(MainController.l.getAncestorScene());
-		primaryStage.show();
+
+	@FXML private Label lblNames;
+    @FXML private Button btnLogOut;
+    @FXML private Button btnMyProfile;
+    @FXML private User user;
+
+    @FXML private UserProfilePanelController userProfilePanelController;
+ 
+ 
+// **************** INTERNAL METHODS *****************************************************
+       
+    
+    
+    
+    @FXML
+    void initialize()
+    {  
+    	user= new User(this);
+    	
+    	System.out.println("JESTEM klasa : " + Main.getActualController());
+    	if(Main.getActualController() instanceof LoginController)
+    	{
+    		user.createAppUser(Main.getMainController().getLoginController().getLogin().getResp());
+    		System.out.println("A1");
+    	}
+    	else if(Main.getActualController() instanceof RegistrationController)
+    	{
+    		user.createAppUser(Main.getMainController().getRegistrationController().getAppUser());
+    		System.out.println("A2");
+    	}
+    }
+    
+    public void logOut(ActionEvent event)
+    {
+    	Main.setAppUser(null);
+        super.backToMainMenu(event);
+    }    
+    
+    public void myProfile(ActionEvent event)
+    {
+    	userProfilePanelController = (UserProfilePanelController) createView("UserProfile",event,this); 	
+    }
+
+	@Override
+	public void update() 
+	{
+		lblNames.setText(user.getAppUser().getName() +" " + user.getAppUser().getSecondName() );		      			
+	}
+
+	public User getUser() {
+		return user;
 	}
 	
 	
