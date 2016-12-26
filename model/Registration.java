10ff6919b0e4016f6c7569648a44e7ef36f2a8be
Marcin Pancerz <marcin2523@o2.pl>
2016-12-26 18:02:45 +0100

    Added registration method

--- model/Registration.java
+++ model/Registration.java
@@ -1,11 +1,146 @@
 package model;
 
-public class Registration extends Panel{
+import org.apache.commons.validator.routines.EmailValidator;
+
+import controler.UserPanelController;
+import mainClasses.AppUser;
+import validation.PasswordHash;
+
+public class Registration extends Model {
 
 	
-	public Registration()
+	AppUser appUser; 
+	
+	
+	
+	public Registration() {
+		
+		super();
+		appUser =new AppUser();
+		
+	}
+	
+	public void register()
 	{
-		super("Registration");
+		System.out.println(appUser);
+		System.out.println(appUser.getPhoneNumber());
+		Main.getClient().executeUserInsert(appUser);
+	
+	}
+	public int setName(String name)
+	{	
+		if(name.length()==0)
+		{
+			return -1;
+		}
+		else
+		{
+			appUser.setName(name);
+			return 1;
+		}
+	}
+	
+	public int setLastName(String lastName)
+	{		
+		if(lastName.length()==0)
+		{
+			return -1;
+		}
+		else
+		{
+			appUser.setSecondName(lastName);
+			return 1;
+		}
+	}
+	
+	public int setEmail(String email)
+	{
+		EmailValidator ev = EmailValidator.getInstance();
+		if(ev.isValid(email))
+		{
+			if(!Main.getClient().isEmailExisting(email))
+			{
+				appUser.setEmail(email);
+				return 1;
+			}
+			else
+			{
+				return -2;			
+			}
+		}
+		else
+		{
+			return -1;
+		}
+		
+
+			
+			
 	}
 
+	public int setPassword(String password1, String password2)
+	{
+		int out=0;
+		
+		//SPRAWDZENIE CZY PODANE HASLA SA IDENTYCZNE
+		if(password1.equals(password2))
+		{
+			
+			if(password1.length()>0 && password1.length()<30)
+			{
+				String passwordHash="";
+					try {
+						passwordHash = PasswordHash.getSaltedHash(password1);
+						System.out.println(passwordHash);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					appUser.setPassword(passwordHash);	
+					out=1;
+			}
+			else
+			{
+				out=-2;
+			}	
+		}
+		else
+		{
+			out=-1;
+		}	
+		return out;
+	}
+	
+	public int setAddress(String address)	
+	{	
+		if(address.length()==0)
+		{
+			return -1;
+		}
+		else
+		{
+			appUser.setAddress(address);
+			return 1;
+		}
+	}
+	
+	public int setPhoneNumber(String phoneNumber)	
+	{	
+		if(phoneNumber.length()==0)
+		{
+			return -1;
+		}
+		else
+		{
+			appUser.setPhoneNumber(phoneNumber);
+			return 1;
+		}
+	}
+
+	public AppUser getAppUser() {
+		return appUser;
+	}
+	
+	
 }
